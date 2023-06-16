3fcb143d3885c13e81141db6cfb9896bc39b411b
Sadi Zeynalov <sadi.zeynalov@cyberhull.com>
2023-06-16 23:05:15 +0400

    removed not working api

--- src/app/top/page.tsx
+++ src/app/top/page.tsx
@@ -1,5 +1,4 @@
 import { Metadata, NextPage } from "next"
-import getTopRatedMovies from "../../../data/getTopRatedMovies"
 
 export const metadata: Metadata = {
     title: 'Top 250 films ever | FilmLibary',
@@ -7,8 +6,6 @@ export const metadata: Metadata = {
   }
 
 const page: NextPage = async () => {
-    const topMovies = await getTopRatedMovies();
-    console.log(topMovies);
     return (
         <h1>Top 100 films ever</h1>
     )
